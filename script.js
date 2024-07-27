d40bba7e7fc1559b6c6667bda196cdbccf272cfc
Najm Ud Deen <149934649+najmuddeen-shigri@users.noreply.github.com>
2024-07-27 18:09:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let num1 = 12;
+let num2 = 45;
+let num3 = 23;
+let num4 = 78;
+let num5 = 34;
+
+let largest = num1;
+
+if(num2 > largest){
+    largest = num2;
+}
+if(num3 > largest){
+    largest = num3;
+}
+if(num4 > largest){
+    largest = num4;
+}
+if(num5 > largest){
+    largest = num5;
+}
+
+
+alert("The largest Number is: " + largest)
